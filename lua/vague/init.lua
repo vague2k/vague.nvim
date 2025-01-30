8c6be7c0da84399120de2036ec7919f84e3ce84d
Alberto Hernandez <albert@Albertos-Laptop.local>
2025-01-30 05:59:35 -0500

    add: `get_palette` func
    would be useful for anyone who wants to get the colorscheme's palette,
    OR use the tiny-devicons-auto-colors plugin

--- lua/vague/init.lua
+++ lua/vague/init.lua
@@ -6,6 +6,15 @@ M.setup = function(user_opts)
   if user_opts then internal_conf.set(user_opts) end
 end
 
+-- SHOULD BE CALLED AFTER SETUP (unless using default colors)
+M.get_palette = function()
+  local palette = {}
+  for _, color in pairs(internal_conf.current.colors) do
+    table.insert(palette, color)
+  end
+  return palette
+end
+
 --- Under the hood, |:colorscheme| is just using |:highlight GroupName ...| over every highlight group it knows about.
 --- so this function is the equivalent to calling |:colorscheme| so use that instead
 M._colorscheme = function()
