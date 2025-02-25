5a36db645d02ea4c9ffe43093aafce6b6addc72f
Alberto Hernandez <121782036+vague2k@users.noreply.github.com>
2025-02-25 12:22:54 -0500

    Snacks Picker support (#21)
    * add: snacks picker support
    
    * seperate snacks input and picker highlights

--- lua/vague/groups/init.lua
+++ lua/vague/groups/init.lua
@@ -12,4 +12,6 @@ return {
   telescope = require("vague.groups.telescope").get_colors(curr_internal_conf),
   treesitter = require("vague.groups.treesitter").get_colors(curr_internal_conf),
   dashboard = require("vague.groups.dashboard").get_colors(curr_internal_conf),
+  snacks_picker = require("vague.groups.snacks-picker").get_colors(curr_internal_conf),
+  snacks_input = require("vague.groups.snacks-input").get_colors(curr_internal_conf),
 }
---
+++ lua/vague/groups/snacks-picker.lua
@@ -0,0 +1,32 @@
+local telescope_group = require("vague.groups.telescope")
+local neotree_group = require("vague.groups.neotree")
+local M = {}
+
+---@param conf VagueColorscheme.InternalConfig
+---@return table
+M.get_colors = function(conf)
+  local telescope = telescope_group.get_colors(conf)
+  local neotree = neotree_group.get_colors(conf)
+
+  -- stylua: ignore
+  local hl = {
+    SnacksPickerMatch              = telescope["TelescopeMatching"],     -- Matching text in picker 
+    SnacksPickerPrompt             = telescope["TelescopePromptPrefix"], -- Prefix in picker prompt
+    SnacksPickerBorder             = telescope["TelescopeBorder"],       -- Border for Telescope
+    SnacksPickerBoxBorder          = telescope["TelescopeBorder"],       -- Border for Telescope
+    SnacksPickerListBorder         = telescope["TelescopeBorder"],       -- Border for Telescope
+    SnacksPickerInputBorder        = telescope["TelescopeBorder"],       -- Border for Telescope
+    SnacksPickerPrewviewBorder     = telescope["TelescopeBorder"],       -- Border for Telescope
+    SnacksPickerCursorLine         = telescope["TelescopeSelection"],    -- Selection in picker
+    SnacksPickerBoxCursorLine      = telescope["TelescopeSelection"],    -- Selection in picker
+    SnacksPickerListCursorLine     = telescope["TelescopeSelection"],    -- Selection in picker
+    SnacksPickerInputCursorLine    = telescope["TelescopeSelection"],    -- Selection in picker
+    SnacksPickerPreviewCursorLine  = telescope["TelescopeSelection"],    -- Selection in picker
+    SnacksPickerGitStatusUntracked = neotree["NeoTreeGitUntracked"],     -- color of untracked git items in a picker (usually explorer)
+    SnacksPickerGitStatusAdded     = neotree["NeoTreeGitAdded"],         -- color of added git items in a picker (usually explorer)
+    SnacksPickerGitStatusModified  = neotree["NeoTreeGitModified"],      -- color of modified git items in a picker (usually explorer)
+  }
+
+  return hl
+end
+return M
