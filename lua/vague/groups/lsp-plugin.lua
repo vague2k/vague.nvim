9d7e33829892590c6acca55852f6bfd69d85dec5
Pete Kazmier <pete@kazmier.com>
2025-05-30 08:25:44 -0400

    feat: add diagnostic_ok highlight groups
    This commit adds the missing DiagnosticOk and similar highlight groups.

--- lua/vague/groups/lsp-plugin.lua
+++ lua/vague/groups/lsp-plugin.lua
@@ -10,13 +10,16 @@ M.get_colors = function(conf)
     DiagnosticError               = { fg = c.error, gui = conf.plugins.lsp.diagnostic_error },   -- diagnostic error
     DiagnosticHint                = { fg = c.hint, gui = conf.plugins.lsp.diagnostic_hint},      -- diagnostic hint
     DiagnosticInfo                = { fg = c.constant, gui = conf.plugins.lsp.diagnostic_info }, -- diagnostic info
+    DiagnosticOk                  = { fg = c.plus, gui = conf.plugins.lsp.diagnostic_ok },       -- diagnostic ok
     DiagnosticUnderlineError      = { gui = "undercurl", sp = c.error },                         -- undercurl for errors
     DiagnosticUnderlineHint       = { gui = "undercurl", sp = c.hint },                          -- undercurl for hints
     DiagnosticUnderlineInfo       = { gui = "undercurl", sp = c.hint },                          -- undercurl for info
+    DiagnosticUnderlineOk         = { gui = "undercurl", sp = c.plus },                          -- undercurl for ok
     DiagnosticUnderlineWarn       = { gui = "undercurl", sp = c.delta },                         -- undercurl for warnings
     DiagnosticVirtualTextError    = { fg = c.error, gui = conf.plugins.lsp.diagnostic_error },   -- virtual text for errors
     DiagnosticVirtualTextHint     = { fg = c.hint, gui = conf.plugins.lsp.diagnostic_hint},      -- virtual text for hints
     DiagnosticVirtualTextInfo     = { fg = c.delta, gui = conf.plugins.lsp.diagnostic_info },    -- virtual text for info
+    DiagnosticVirtualTextOk       = { fg = c.plus, gui = conf.plugins.lsp.diagnostic_ok },       -- virtual text for ok
     DiagnosticVirtualTextWarn     = { fg = c.warning, gui = conf.plugins.lsp.diagnostic_warn },  -- virtual text for warnings
     DiagnosticWarn                = { fg = c.warning, gui = conf.plugins.lsp.diagnostic_warn},   -- diagnostic warning
     LspCodeLens                   = { fg = c.comment, gui = conf.style.comments },               -- code lens text
