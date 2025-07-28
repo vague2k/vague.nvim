698c9d2002ab0a05b96e1b79bb6661645cc9a8f7
skewb1k <skewb1kunix@gmail.com>
2025-07-28 05:28:42 +0300

    feat: add missing builtin groups: Added, Changed, Removed

--- lua/vague/groups/diff.lua
+++ lua/vague/groups/diff.lua
@@ -8,12 +8,15 @@ M.get_colors = function(conf)
 
   -- stylua: ignore
   local hl = {
-    DiffAdd          = { fg = c.plus, bg = utilities.blend(c.plus, c.bg, 0.2) },
-    DiffChange       = { fg = c.delta, bg = utilities.blend(c.delta, c.bg, 0.2) },
-    DiffDelete       = { fg = c.error, bg = utilities.blend(c.error, c.bg, 0.2) },
-    DiffText         = { fg = c.fg },
-    DiffFile         = { fg = c.keyword },
-    DiffIndexLine    = { fg = c.comment },
+    Added         = { fg = c.plus },
+    Changed       = { fg = c.delta },
+    Removed       = { fg = c.error },
+    DiffAdd       = { fg = c.plus, bg = utilities.blend(c.plus, c.bg, 0.2) },
+    DiffChange    = { fg = c.delta, bg = utilities.blend(c.delta, c.bg, 0.2) },
+    DiffDelete    = { fg = c.error, bg = utilities.blend(c.error, c.bg, 0.2) },
+    DiffText      = { fg = c.fg },
+    DiffFile      = { fg = c.keyword },
+    DiffIndexLine = { fg = c.comment },
   }
 
   return hl
