ab73d180af05eee0aaa25e9e6a8bf374c93c32c6
skewb1k <skewb1kunix@gmail.com>
2025-07-28 22:11:16 +0300

    feat: add missing builtin groups: Added, Changed, Removed (#50)
    * feat: add missing builtin groups: Added, Changed, Removed
    
    * fix: remove redundant linking of mini-diff groups

--- lua/vague/groups/mini.lua
+++ lua/vague/groups/mini.lua
@@ -1,17 +1,12 @@
-local diff_group = require("vague.groups.diff")
 local M = {}
 
 ---@param conf VagueColorscheme.InternalConfig
 ---@return table
 M.get_colors = function(conf)
   local c = conf.colors
-  local diff = diff_group.get_colors(conf)
 
   -- stylua: ignore
   local hl = {
-    MiniDiffOverAdd     = diff["DiffAdd"],
-    MiniDiffOverChange  = diff["DiffChange"],
-    MiniDiffOverDelete  = diff["DiffDelete"],
     MiniDiffOverContext = { bg = c.line },
     MiniDiffSignAdd     = { fg = c.plus },
     MiniDiffSignChange  = { fg = c.delta },
