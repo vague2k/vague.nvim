cf1499a2b9d523d77cbb7079942488f67107f40b
skewb1k <skewb1kunix@gmail.com>
2025-08-05 13:17:55 +0300

    feat: migrate to vim.api.nvim_set_hl()
    Replace string-formatted :highlight command with the native Lua API vim.api.nvim_set_hl().
    This change improves performance by avoiding command-string construction and enables
    full support for all keys supported in vim.api.keyset.highlight.

--- lua/vague/highlights.lua
+++ lua/vague/highlights.lua
@@ -2,24 +2,10 @@ local curr_internal_conf = require("vague.config.internal").current
 local groups = require("vague.groups")
 local M = {}
 
----@param highlights table <string, table>
+---@param highlights table <string, vim.api.keyset.highlight>
 local function set_vim_highlights(highlights)
-  for name, setting in pairs(highlights) do
-    if setting.gui == "bold" and not curr_internal_conf.bold then
-      setting.gui = "none"
-    elseif setting.gui == "italic" and not curr_internal_conf.italic then
-      setting.gui = "none"
-    end
-    vim.api.nvim_command(
-      string.format(
-        "highlight %s guifg=%s guibg=%s guisp=%s gui=%s",
-        name,
-        setting.fg or "none",
-        setting.bg or "none",
-        setting.sp or "none",
-        setting.gui or "none"
-      )
-    )
+  for name, highlight in pairs(highlights) do
+	vim.api.nvim_set_hl(0, name, highlight)
   end
 end
 
