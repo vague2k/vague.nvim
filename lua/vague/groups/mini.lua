0442e4fb3abb2cec49783310aaa1869f0b280964
dave <davepeix@gmail.com>
2025-09-05 21:25:56 -0300

    feat: support mini.statusline plugin

--- lua/vague/groups/mini.lua
+++ lua/vague/groups/mini.lua
@@ -13,6 +13,17 @@ M.get_colors = function(conf)
     MiniDiffSignDelete  = { fg = c.error },
 
     MiniTrailspace      = { bg = c.error },
+
+    MiniStatuslineModeNormal  = { fg = c.bg, bg = c.operator, gui = "bold" },
+    MiniStatuslineModeInsert  = { fg = c.bg, bg = c.delta, gui = "bold" },
+    MiniStatuslineModeVisual  = { fg = c.bg, bg = c.builtin, gui = "bold" },
+    MiniStatuslineModeReplace = { fg = c.bg, bg = c.string, gui = "bold" },
+    MiniStatuslineModeCommand = { fg = c.bg, bg = c.string, gui = "bold" },
+    MiniStatuslineModeOther   = { fg = c.bg, bg = c.string, gui = "bold" },
+    MiniStatuslineDevinfo     = { fg = c.fg, bg = c.inactiveBg },
+    MiniStatuslineFilename    = { fg = c.comment, bg = c.bg },
+    MiniStatuslineFileinfo    = { fg = c.fg, bg = c.inactiveBg },
+    MiniStatuslineInactive    = { fg = c.comment, bg = c.bg },
   }
 
   return hl
